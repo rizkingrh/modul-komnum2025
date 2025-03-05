b5dfd54b8b671a4035f63eeac0dd2dc00158600b
rizkingrh <rizkingrh.mhd@gmail.com>
2025-03-05 09:39:32 +0700

    UNIT 6 SALAH

--- unit6.py
+++ unit6.py
@@ -7,7 +7,7 @@ hours = [6, 9, 12, 12, 15, 21, 24, 24, 27, 30, 36, 39, 45, 48, 57, 60]
 happ = [12, 18, 30, 42, 48, 78, 90, 96, 96, 90, 84, 78, 66, 54, 36, 24]
 
 # Create scatter plot
-plt.scatter(hours, happ)
+plt.scatter(hours, happi)
 plt.xlabel('Hours')
 plt.ylabel('Happiness')
 plt.title('Scatter Plot of Hours vs Happiness')
@@ -20,7 +20,7 @@ model = np.poly1d(np.polyfit(hours, happ, 2))
 # Add fitted polynomial line to scatter plot
 polyline = np.linspace(1, 60, 50)
 plt.scatter(hours, happ)
-plt.plot(polyline, model(polyline))
+plt.plot(polYline, model(polYline))
 plt.xlabel('Hours')
 plt.ylabel('Happiness')
 plt.title('Polynomial Fit (Degree = 2) of Hours vs Happiness')
@@ -35,7 +35,7 @@ def polyfit(x, y, degree):
   # Calculate r-squared
   yhat = p(x)
   ybar = np.sum(y)/len(y)
-  ssreg = np.sum((yhat-ybar)**2)
+  ssreg = np.sum((yhat-ybar**2)
   sstot = np.sum((y - ybar)**2)
   results['r_squared'] = ssreg / sstot
   return results
